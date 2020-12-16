8d5d4ee130ff26a7f9b8cd2c4261a96e8c4075aa
devops-brain <31346387+devops-brain@users.noreply.github.com>
2020-12-16 01:43:28 -0500

    tuning io blocking script and ceph jenkins cleanup

--- k8s/juju-block-io.sh
+++ k8s/juju-block-io.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/env bash
 
-until [ "$(juju show-status kubernetes-master/0 | grep kubernetes-master/0 | awk '{print $2}')" = "active" ]
-do
-  sleep 1m
-done
 
-sleep 6s
-until [ "$(juju show-status kubernetes-master/0 | grep kubernetes-master/0 | awk '{print $2}')" = "active" ]
-do
-  sleep 1m
-done
 
-sleep 6s
-until [ "$(juju show-status kubernetes-master/0 | grep kubernetes-master/0 | awk '{print $2}')" = "active" ]
+for i in `seq 1 15`
 do
-  sleep 1m
-done
-
-sleep 6s
-until [ "$(juju show-status kubernetes-master/0 | grep kubernetes-master/0 | awk '{print $2}')" = "active" ]
-do
-  sleep 1m
+  sleep 1s
+  until [ "$(juju show-status kubernetes-master/0 | grep kubernetes-master/0 | awk '{print $2}')" = "active" ]
+  do
+    sleep 1s
+  done
 done
