6828032e23bb935d22aa4b69a6b64b6bf291433c
devops-brain <31346387+devops-brain@users.noreply.github.com>
2021-01-11 01:59:38 -0500

    tuning juju io block code

--- k8s/juju-block-io.sh
+++ k8s/juju-block-io.sh
@@ -4,7 +4,7 @@
 for i in `seq 1 15`
 do
   sleep 1s
-  until ["$(juju status | grep 192.168. | grep '/' | awk '{print $3}' | sort | uniq | wc -l)" = "1"]
+  until ["$(juju status | grep 192.168. | grep '/' | awk '{print $3}' | sort | uniq | wc -l)" == "1"]
   do
     #echo $(juju status | grep 192.168. | grep '/' | awk '{print $3}' | sort | uniq | wc -l)
     #echo $(juju status | grep 192.168. | grep '/' | awk '{print $3}' | sort | uniq)
