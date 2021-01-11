7015160a35218285dbae741ae9459663aa765dc5
devops-brain <31346387+devops-brain@users.noreply.github.com>
2021-01-11 02:05:18 -0500

    tuning juju io block code

--- k8s/juju-block-io.sh
+++ k8s/juju-block-io.sh
@@ -4,13 +4,11 @@
 for i in `seq 1 15`
 do
   sleep 1s
-  echo $(i)
-  #echo $(juju status | grep 192.168. | grep '/' | awk '{print $3}' | sort | uniq | wc -l)
-  #echo $(juju status | grep 192.168. | grep '/' | awk '{print $3}' | sort | uniq)
+  echo ${i}
+  echo $(juju status | grep 192.168. | grep '/' | awk '{print $3}' | sort | uniq | wc -l)
+  echo $(juju status | grep 192.168. | grep '/' | awk '{print $3}' | sort | uniq)
   until [ "$(juju status | grep 192.168. | grep '/' | awk '{print $3}' | sort | uniq | wc -l)" == "1" ]
   do
-    #echo $(juju status | grep 192.168. | grep '/' | awk '{print $3}' | sort | uniq | wc -l)
-    #echo $(juju status | grep 192.168. | grep '/' | awk '{print $3}' | sort | uniq)
     until [ "$(juju status | grep 192.168. | grep '/' | awk '{print $3}' | sort | uniq)" == "idle" ]
     do
       echo $(juju status | grep 192.168. | grep '/' | awk '{print $3}' | sort | uniq | wc -l)
